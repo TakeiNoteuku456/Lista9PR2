94a18863de4f38ce716912a84d587ebfa578d5d3
Takei_Noteuku456 <126687468+TakeiNoteuku456@users.noreply.github.com>
2023-05-31 15:55:20 -0300

    Update Program.cs

--- Lista9PR2/Program.cs
+++ Lista9PR2/Program.cs
@@ -10,221 +10,185 @@ namespace Lista9PR2
     {
         static void ImprimirVetor(int[] vetor, int tamanho)
         {
+             int opcao;
+
+            do
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Exercício 1");
+                Console.WriteLine("2. Exercício 2");
+                Console.WriteLine("3. Exercício 3");
+                Console.WriteLine("4. Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Exercicio1();
+                        break;
+                    case 2:
+                        Exercicio2();
+                        break;
+                    case 3:
+                        Exercicio3();
+                        break;
+                    case 4:
+                        Exercicio4();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (opcao != 4);
+        }
+
+        static void Exercicio1()
+        {
+            Console.Write("Informe o tamanho do vetor: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+
+            int[] vetor = new int[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                vetor[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Vetor:");
             for (int i = 0; i < tamanho; i++)
             {
                 Console.WriteLine(vetor[i]);
             }
+
+            int soma = 0;
+            for (int i = 0; i < tamanho; i++)
+            {
+                soma += vetor[i];
+            }
+            double media = (double)soma / tamanho;
+
+            Console.WriteLine("Média: " + media);
+
+            Console.WriteLine("Executando o exercício 1...");
+        }
+
+        static void Exercicio2()
+        {
+            Console.Write("Informe o tamanho do vetor: ");
+            int tamanho1 = int.Parse(Console.ReadLine());
+
+            Random random1 = new Random();
+
+            int[] vetor1 = new int[tamanho1];
+
+            for (int i = 0; i < tamanho1; i++)
+            {
+                vetor1[i] = random1.Next(1, 101);
+            }
+
+            Console.WriteLine("Vetor:");
+            for (int i = 0; i < tamanho1; i++)
+            {
+                Console.WriteLine(vetor1[i]);
+            }
+
+            int somaPares = 0;
+            int somaImpares = 0;
+            for (int i = 0; i < tamanho1; i++)
+            {
+                if (vetor1[i] % 2 == 0)
+                {
+                    somaPares += vetor1[i];
+                }
+                else
+                {
+                    somaImpares += vetor1[i];
+                }
+            }
+
+            Console.WriteLine("Soma dos valores pares: " + somaPares);
+            Console.WriteLine("Soma dos valores ímpares: " + somaImpares);
+
+            Console.WriteLine("Executando o exercício 2...");
+        }
+
+        static void Exercicio3()
+        {
+            Console.Write("Informe o tamanho do vetor: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+
+            int[] vetor = new int[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                vetor[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Vetor de dados da posição 0 para a última:");
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+
             Console.WriteLine();
+
+            Console.WriteLine("Vetor de dados da última para a posição 0:");
+
+            for (int i = tamanho - 1; i >= 0; i--)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+            Console.WriteLine("Executando o exercício 3...");
         }
-
-        // Função auxiliar para ordenar o vetor em ordem crescente
-        static void OrdenarVetorCrescente(int[] vetor, int tamanho)
+        static void Exercicio4()
         {
-            for (int i = 0; i < tamanho - 1; i++)
+            Console.Write("Informe o tamanho do vetor: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+
+            int[] vetor = new int[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
             {
-                for (int j = i + 1; j < tamanho; j++)
+                vetor[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Valores do vetor:");
+            for (int i = 0; i < tamanho; i++)
+            {
+                Console.Write(vetor[i] + " ");
+            }
+            Console.WriteLine();
+
+            int soma = 0;
+            int contador = 0;
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                if (vetor[i] % 3 == 0 && vetor[i] % 5 == 0)
                 {
-                    if (vetor[j] < vetor[i])
-                    {
-                        int temp = vetor[i];
-                        vetor[i] = vetor[j];
-                        vetor[j] = temp;
-                    }
-                }
-            }
-        }
-
-        // Função auxiliar para inverter o vetor
-        static void InverterVetor(int[] vetor, int tamanho)
-        {
-            int i = 0;
-            int j = tamanho - 1;
-            while (i < j)
-            {
-                int temp = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = temp;
-                i++;
-                j--;
-            }
-        }
-        static void Main(string[] args)
-
-
-        {
-
-            int Ex;
-
-            Console.WriteLine("========Menu========");
-            Console.WriteLine("====Exercício 1=====");
-            Console.WriteLine("====Exercício 2=====");
-            Console.WriteLine("====Exercício 3=====");
-            Console.WriteLine("====Exercício 4=====");
-
-            Ex = int.Parse(Console.ReadLine());
-
-            switch (Ex)
-            {
-                case 1:
-                    Console.Write("Informe o tamanho do vetor: ");
-                    int tamanho = int.Parse(Console.ReadLine());
-
-                    // Cria um objeto Random para gerar valores aleatórios
-                    Random random = new Random();
-
-                    // Cria o vetor com o tamanho informado pelo usuário
-                    int[] vetor = new int[tamanho];
-
-                    // Preenche o vetor com valores aleatórios
-                    for (int i = 0; i < tamanho; i++)
-                    {
-                        // Gera um valor aleatório entre 1 e 100
-                        vetor[i] = random.Next(1, 101);
-                    }
-
-                    // Imprime o vetor
-                    Console.WriteLine("Vetor:");
-                    for (int i = 0; i < tamanho; i++)
-                    {
-                        Console.WriteLine(vetor[i]);
-                    }
-
-                    // Calcula a média aritmética dos valores no vetor
-                    int soma = 0;
-                    for (int i = 0; i < tamanho; i++)
-                    {
-                        soma += vetor[i];
-                    }
-                    double media = (double)soma / tamanho;
-
-                    // Imprime a média aritmética
-                    Console.WriteLine("Média: " + media);
-                    break;
-                case 2:
-                    Console.Write("Informe o tamanho do vetor: ");
-                    int tamanho1 = int.Parse(Console.ReadLine());
-
-                    // Cria um objeto Random para gerar valores aleatórios
-                    Random random1 = new Random();
-
-                    // Cria o vetor com o tamanho informado pelo usuário
-                    int[] vetor1 = new int[tamanho1];
-
-                    // Preenche o vetor com valores aleatórios
-                    for (int i = 0; i < tamanho1; i++)
-                    {
-                        // Gera um valor aleatório entre 1 e 100
-                        vetor1[i] = random1.Next(1, 101);
-                    }
-
-                    // Imprime o vetor
-                    Console.WriteLine("Vetor:");
-                    for (int i = 0; i < tamanho1; i++)
-                    {
-                        Console.WriteLine(vetor1[i]);
-                    }
-
-                    // Calcula a soma dos valores pares e ímpares
-                    int somaPares = 0;
-                    int somaImpares = 0;
-                    for (int i = 0; i < tamanho1; i++)
-                    {
-                        if (vetor1[i] % 2 == 0) // Verifica se o valor é par
-                        {
-                            somaPares += vetor1[i];
-                        }
-                        else
-                        {
-                            somaImpares += vetor1[i];
-                        }
-                    }
-
-                    // Imprime a soma dos valores pares e ímpares
-                    Console.WriteLine("Soma dos valores pares: " + somaPares);
-                    Console.WriteLine("Soma dos valores ímpares: " + somaImpares);
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-                    Console.Write("Informe o tamanho do vetor: ");
-                    int tamanho = int.Parse(Console.ReadLine());
-
-                    // Cria um objeto Random para gerar valores aleatórios
-                    Random random = new Random();
-
-                    // Cria o vetor com o tamanho informado pelo usuário
-                    int[] vetor = new int[tamanho];
-
-                    // Preenche o vetor com valores aleatórios
-                    for (int i = 0; i < tamanho; i++)
-                    {
-                        // Gera um valor aleatório entre 1 e 100
-                        vetor[i] = random.Next(1, 101);
-                    }
-
-                    // Imprime o vetor gerado
-                    Console.WriteLine("Vetor original:");
-                    ImprimirVetor(vetor, tamanho);
-
-                    // Ordena o vetor em ordem crescente
-                    OrdenarVetorCrescente(vetor, tamanho);
-
-                    // Imprime o vetor em ordem crescente
-                    Console.WriteLine("Vetor em ordem crescente:");
-                    ImprimirVetor(vetor, tamanho);
-
-                    // Inverte o vetor para ordem decrescente
-                    InverterVetor(vetor, tamanho);
-
-                    // Imprime o vetor em ordem decrescente
-                    Console.WriteLine("Vetor em ordem decrescente:");
-                    ImprimirVetor(vetor, tamanho);
-            }
-
-            // Função auxiliar para imprimir o vetor
-            static void ImprimirVetor(int[] vetor, int tamanho)
-            {
-                for (int i = 0; i < tamanho; i++)
-                {
-                    Console.WriteLine(vetor[i]);
-                }
-                Console.WriteLine();
-            }
-
-            // Função auxiliar para ordenar o vetor em ordem crescente
-            static void OrdenarVetorCrescente(int[] vetor, int tamanho)
-            {
-                for (int i = 0; i < tamanho - 1; i++)
-                {
-                    for (int j = i + 1; j < tamanho; j++)
-                    {
-                        if (vetor[j] < vetor[i])
-                        {
-                            int temp = vetor[i];
-                            vetor[i] = vetor[j];
-                            vetor[j] = temp;
-                        }
-                    }
+                    soma += vetor[i];
+                    contador++;
                 }
             }
 
-            // Função auxiliar para inverter o vetor
-            static void InverterVetor(int[] vetor, int tamanho)
-            {
-                int i = 0;
-                int j = tamanho - 1;
-                while (i < j)
-                {
-                    int temp = vetor[i];
-                    vetor[i] = vetor[j];
-                    vetor[j] = temp;
-                    i++;
-                    j--;
-                }
-            }
-            break;
+            double media = contador > 0 ? (double)soma / contador : 0;
+
+            Console.WriteLine("Média aritmética dos valores divisíveis por 3 e por 5: " + media);
+
+    Console.WriteLine("Executando o exercício 4...");
         }
     }
 }
- 
